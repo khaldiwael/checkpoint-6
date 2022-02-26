bec9ecc92c3004fde27c2dcceb19b690c06ef52c
Your Name <you@example.com>
2022-02-26 01:29:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
+import './App.css';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import Address from './Component/address';
+// import FullName from './Component/fullName';
+// import ProfilePhoto from './Component/profilePhoto';
+import Address from './component/address'
+import FullName from './component/fullName'
+import ProfilePhoto from './component/profilePhoto';
 
+class App extends React.Component{
+  state ={
+    fullName:"wael",
+    bio:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique consequatur totam perspiciatis. Distinctio maiores id reiciendis, iusto qui exercitationem quasi perferendis! Quibusdam perspiciatis quos quas ullam. Laudantium aliquam corporis iusto.",
+    imgSrc:"",
+    profession:"emploi",
+    show:'profileA'
+  };
+
+  handleClick =() =>{ if (this.state.show=== 'profileA')
+                        this.setState({show:'profileDisplay'})
+                      else this.setState({show:'profileA'})
+  }
+  render() {
+    return(
+      <>
+        <h1>Profile</h1>
+        <button  type='button' onClick={this.handleClick}>show profile</button>
+        <div className={this.state.show}>
+          <h1>show:</h1>
+                  
+            <div className='main'>
+              
+              <div>
+                <ProfilePhoto/>
+              </div>
+              <div>
+                <FullName/>
+                <Address/>
+              </div>
+              
+            </div>
+        </div>
+     </>
+      )
+  }
+}
 export default App;
